16d14d008e9796a5f11f3d1e871f1fb11ee53055
Ameer Deen <writeameer@gmail.com>
2023-01-18 12:19:17 +0400

    Added GetUpdateStatus

---
+++ PulumiApi/Models/GetUpdateStatusResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PulumiApi.Models;
+
+public class GetUpdateStatusResponse
+{
+    [JsonPropertyName("status")]
+    public string? Status { get; set; } 
+
+    //[JsonPropertyName("events")]
+    //public Dictionary<string, object> Events {get;set;} = new Dictionary<string, object>();
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this, new JsonSerializerOptions{WriteIndented = true });
+    }
+}
