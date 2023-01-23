c72ed2acc88b2f0d2e96d4bd12d38f37a0113ecf
Ameer Deen <writeameer@gmail.com>
2023-01-23 18:19:58 +0400

    Added test for getting info on latest stack update

--- PulumiApi/publish.ps1
+++ PulumiApi/publish.ps1
@@ -1,4 +1,17 @@
-﻿dotnet build -c release
-
+﻿
+# Remove any existing packages
 $packageName = (Get-ChildItem -r -Path .\bin\release\*.nupkg| Select-Object FullName).FullName
+$packageName | % { 
+	if ($_ -ne $null) {
+		rm $_ 
+	}
+}
+
+# Publish new package
+dotnet publish -c release
+
+# Find package Full Name
+$packageName = (Get-ChildItem -r -Path .\bin\release\*.nupkg| Select-Object FullName).FullName
+
+# Publish it
 dotnet nuget push "$packageName" --api-key "$env:NUGET_API_KEY" --source https://api.nuget.org/v3/index.json
